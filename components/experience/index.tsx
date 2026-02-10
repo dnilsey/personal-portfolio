9163b921b6c350c28757d44bfc607e33a790a8a9
dnilsey <d.nilsey@gmail.com>
2026-02-10 20:11:06 +0900

    Update index.tsx

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -6,7 +6,7 @@ import { classNames } from "@/lib/classnames";
 import { useEffect, useRef, useState } from "react";
 import GenericDropdown from "../shared/inputs/GenericDropdown";
 import WithNavLayout from "../shared/layouts/WithNavLayout";
-import DesktopStepper from "./DesktopStepper";
+import DesktopStepper, { steps } from "./DesktopStepper";
 
 const items = [
   {
